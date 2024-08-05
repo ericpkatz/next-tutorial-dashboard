179ef51af7db217ee52344c06d9cea35c00257fc
ericpkatz <ericpkatz@gmail.com>
2024-08-05 08:28:03 -0400

    auth middleware

--- app/ui/skeletons.tsx
+++ app/ui/skeletons.tsx
@@ -3,7 +3,6 @@ const shimmer =
   'before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/60 before:to-transparent';
 
 export function CardSkeleton() {
-  console.log('skelly');
   return (
     <div
       className={`${shimmer} relative overflow-hidden rounded-xl bg-gray-100 p-2 shadow-sm`}
